17d190fc3573a41dea1ed6f8d169a9e737ace050
Sven Nebel <nebel.sven@gmail.com>
2019-03-10 22:50:22 +0000

    Refactor to allow testing

--- internal/pkg/kwatchman/kwatchman.go
+++ internal/pkg/kwatchman/kwatchman.go
@@ -1,11 +1,23 @@
 package kwatchman
 
 import (
-	"github.com/snebel29/kwatchman/internal/pkg/cli"
+	log "github.com/sirupsen/logrus"
 	"github.com/snebel29/kwatchman/internal/pkg/watcher"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func Run(c *cli.CLIArgs) error {
-	w := watcher.NewWatcher(c)
+func Run(w watcher.Watcher) error {
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		sig := <-shutdown
+		log.Infof("Shutdown upon signal %s", sig.Signal)
+		w.Shutdown()
+		os.Exit(0)
+	}()
+
 	return w.Run()
 }
