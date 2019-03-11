25cca013c5a69b3f95aef42fae1b5e718fba786f
Sven Nebel <nebel.sven@gmail.com>
2019-03-11 18:11:33 +0000

    Improve watcher.getK8sClient tests

--- internal/pkg/watcher/watcher_test.go
+++ internal/pkg/watcher/watcher_test.go
@@ -1,20 +1,29 @@
 package watcher
 
 import (
+	"k8s.io/client-go/rest"
+	"os"
 	"path"
 	"runtime"
 	"testing"
 )
 
 func TestGetK8sClient(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	kubeconfig := path.Join(path.Dir(filename), "fixtures", "kubeconfig")
+	// Test InClusterConfig() simulating being within k8s cluster
+	// https://github.com/snebel29/kwatchman/blob/master/vendor/k8s.io/client-go/rest/config.go#L315-L345
+	os.Setenv("KUBERNETES_SERVICE_HOST", "anyValue")
+	os.Setenv("KUBERNETES_SERVICE_PORT", "anyValue")
+	if _, err := getK8sClient(""); err == rest.ErrNotInCluster {
+		t.Error("getK8sClientInCluster() should behave like living within cluster")
+	}
+	os.Unsetenv("KUBERNETES_SERVICE_HOST")
+	os.Unsetenv("KUBERNETES_SERVICE_PORT")
 
+	// Test getK8sClientOutCluster()
+	_, thisFilename, _, _ := runtime.Caller(0)
+	kubeconfig := path.Join(path.Dir(thisFilename), "fixtures", "kubeconfig")
 	_, err := getK8sClient(kubeconfig)
 	if err != nil {
-		t.Errorf("Failed to get client from %s: %v", kubeconfig, err)
+		t.Errorf("Failed to get client from kubeconfig %s: %v", kubeconfig, err)
 	}
-	//TODO: Test rest.InClusterConfig() - Actually is hard to do
-	//since that function uses hardcoded /var/run paths that requires
-	//privileged permissions, the function can't be easily tested
 }
