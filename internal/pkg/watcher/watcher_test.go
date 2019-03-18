d4a2f741e37c1f68ea9a69d52a9e378bc567ac42
Sven Nebel <nebel.sven@gmail.com>
2019-03-18 18:11:21 +0000

    Fix watcher tests

--- internal/pkg/watcher/watcher_test.go
+++ internal/pkg/watcher/watcher_test.go
@@ -1,12 +1,20 @@
 package watcher
 
 import (
+	"github.com/snebel29/kwatchman/internal/pkg/cli"
 	"k8s.io/client-go/rest"
 	"os"
 	"path"
 	"runtime"
 	"testing"
 )
+
+var thisFilename string
+
+func init() {
+	_, t, _, _ := runtime.Caller(0)
+	thisFilename = t
+}
 
 func TestGetK8sClient(t *testing.T) {
 	// Test InClusterConfig() simulating being within k8s cluster
@@ -20,7 +28,6 @@ func TestGetK8sClient(t *testing.T) {
 	os.Unsetenv("KUBERNETES_SERVICE_PORT")
 
 	// Test getK8sClientOutCluster()
-	_, thisFilename, _, _ := runtime.Caller(0)
 	kubeconfig := path.Join(path.Dir(thisFilename), "fixtures", "kubeconfig")
 	_, err := getK8sClient(kubeconfig)
 	if err != nil {
@@ -28,7 +35,74 @@ func TestGetK8sClient(t *testing.T) {
 	}
 }
 
+func TestNewK8sWatcher(t *testing.T) {
+	kubeconfig := path.Join(path.Dir(thisFilename), "fixtures", "kubeconfig")
+	c := &cli.CLIArgs{Kubeconfig: kubeconfig}
+	w, err := NewK8sWatcher(c)
+
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+	if w.ctx == nil {
+		t.Errorf("K8sWatcher.ctx is not set correctly %#v", w.ctx)
+	}
+	if w.cancel == nil {
+		t.Errorf("K8sWatcher.cancel is not set correctly %#v", w.cancel)
+	}
+	if w.opts != c {
+		t.Errorf("K8sWatcher.opts is not set correctly %#v", w.opts)
+	}
+	if w.clientset == nil {
+		t.Errorf("K8sWatcher.clientset is not set correctly %#v", w.clientset)
+	}
+}
+
+type ResourceWatcherMock struct {
+	RunCalled       bool
+	ShutdownCalled  bool
+	HasSyncedCalled bool
+}
+
+func (w *ResourceWatcherMock) Run() error {
+	w.RunCalled = true
+	return nil
+}
+
+func (w *ResourceWatcherMock) Shutdown() {
+	w.ShutdownCalled = true
+}
+
+func (w *ResourceWatcherMock) HasSynced() bool {
+	w.HasSyncedCalled = true
+	return true
+}
+
 func TestK8sWatcher(t *testing.T) {
-	// TODO: Test Run()
-	// TODO: Test Shutdown()
+	cancelCalled := false
+	w := &K8sWatcher{
+		ctx:       nil,
+		cancel:    func() { cancelCalled = true },
+		opts:      nil,
+		clientset: nil,
+		k8sResources: []ResourceWatcher{
+			&ResourceWatcherMock{},
+			&ResourceWatcherMock{},
+			&ResourceWatcherMock{},
+		},
+	}
+
+	w.Run()
+
+	for i, rwi := range w.k8sResources {
+		rw := rwi.(*ResourceWatcherMock)
+		if !rw.RunCalled {
+			t.Errorf("ResourceWatcherMock %v Run() should have been called %#v", i, rw)
+		}
+
+	}
+
+	w.Shutdown()
+	if !cancelCalled {
+		t.Error("watcher.cancel() should have been called")
+	}
 }
