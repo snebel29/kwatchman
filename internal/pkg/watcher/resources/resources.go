8859614af73abac3acf132621315ee4e65644859
Sven Nebel <nebel.sven@gmail.com>
2019-04-21 22:47:17 +0100

    Refactor to start using kooper fork

--- internal/pkg/watcher/resources/resources.go
+++ internal/pkg/watcher/resources/resources.go
@@ -1,74 +1,31 @@
 package resources
 
 import (
-	"context"
-	"errors"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
+	"fmt"
+	_ "github.com/snebel29/kooper/operator/common"
+	_ "github.com/snebel29/kooper/operator/controller"
+	_ "github.com/snebel29/kooper/operator/handler"
+	_ "github.com/snebel29/kooper/operator/retrieve"
 )
-
-type Event struct {
-	key       string
-	action    string
-	namespace string
-	kind      string
-}
 
 type ResourceWatcher interface {
 	Run() error
 	Shutdown()
-	HasSynced() bool
 }
 
 type K8sResourceWatcher struct {
-	ctx      context.Context
-	kind     string
-	informer cache.SharedInformer
-	queue    workqueue.RateLimitingInterface
+	kind string
 }
-
-/*
-func (r *K8sResourceWatcher) PopItem() {
-	newEvent, quit := r.queue.Get()
-
-	if quit {
-		return false
-	}
-	defer r.queue.Done(newEvent)
-	err := r.processItem(newEvent.(Event))
-	if err == nil {
-		// No error, reset the ratelimit counters
-		r.queue.Forget(newEvent)
-	} else if r.queue.NumRequeues(newEvent) < maxRetries {
-		r.logger.Errorf("Error processing %s (will retry): %v", newEvent.(Event).key, err)
-		r.queue.AddRateLimited(newEvent)
-	} else {
-		// err != nil and too many retries
-		r.logger.Errorf("Error processing %s (giving up): %v", newEvent.(Event).key, err)
-		r.queue.Forget(newEvent)
-		utilruntime.HandleError(err)
-	}
-
-	return true
-}
-*/
 
 func (r *K8sResourceWatcher) Run() error {
-	stopCh := make(chan struct{})
-	defer r.Shutdown(stopCh)
-
-	go r.informer.Run(stopCh)
-	if !cache.WaitForCacheSync(stopCh, r.HasSynced) {
-		return errors.New("Timeout waiting for caches to sync")
-	}
+	fmt.Printf("Run K8sResourceWatcher with kind %v", r.kind)
 	return nil
 }
 
-func (r *K8sResourceWatcher) Shutdown(stopCh chan<- struct{}) {
-	r.queue.ShutDown()
-	close(stopCh)
+func (r *K8sResourceWatcher) Shutdown() {
+	fmt.Printf("Shutdown K8sResourceWatcher with kind %v", r.kind)
 }
 
-func (r *K8sResourceWatcher) HasSynced() bool {
-	return r.informer.HasSynced()
+func NewK8sDeploymentWatcher() ResourceWatcher {
+	return &K8sResourceWatcher{kind: "Deployment"}
 }
--- internal/pkg/watcher/resources/resources_test.go
+++ internal/pkg/watcher/resources/resources_test.go
@@ -3,7 +3,5 @@ package resources
 import "testing"
 
 func TestK8sResourceWatcher(t *testing.T) {
-	// TODO: Test Run()
-	// TODO: Test Shutdown()
-	// TODO: Test HasSynced()
+
 }
