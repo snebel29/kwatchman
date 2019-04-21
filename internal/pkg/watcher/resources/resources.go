40640ce6ae9cd37672ffec8411cb062959db457a
Sven Nebel <nebel.sven@gmail.com>
2019-04-21 21:20:05 +0100

    Working changes before switch to kooper

--- internal/pkg/watcher/resources/resources.go
+++ internal/pkg/watcher/resources/resources.go
@@ -2,6 +2,7 @@ package resources
 
 import (
 	"context"
+	"errors"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 )
@@ -26,14 +27,48 @@ type K8sResourceWatcher struct {
 	queue    workqueue.RateLimitingInterface
 }
 
-func (w *K8sResourceWatcher) Run() error {
+/*
+func (r *K8sResourceWatcher) PopItem() {
+	newEvent, quit := r.queue.Get()
+
+	if quit {
+		return false
+	}
+	defer r.queue.Done(newEvent)
+	err := r.processItem(newEvent.(Event))
+	if err == nil {
+		// No error, reset the ratelimit counters
+		r.queue.Forget(newEvent)
+	} else if r.queue.NumRequeues(newEvent) < maxRetries {
+		r.logger.Errorf("Error processing %s (will retry): %v", newEvent.(Event).key, err)
+		r.queue.AddRateLimited(newEvent)
+	} else {
+		// err != nil and too many retries
+		r.logger.Errorf("Error processing %s (giving up): %v", newEvent.(Event).key, err)
+		r.queue.Forget(newEvent)
+		utilruntime.HandleError(err)
+	}
+
+	return true
+}
+*/
+
+func (r *K8sResourceWatcher) Run() error {
+	stopCh := make(chan struct{})
+	defer r.Shutdown(stopCh)
+
+	go r.informer.Run(stopCh)
+	if !cache.WaitForCacheSync(stopCh, r.HasSynced) {
+		return errors.New("Timeout waiting for caches to sync")
+	}
 	return nil
 }
 
-func (w *K8sResourceWatcher) Shutdown() {
-
+func (r *K8sResourceWatcher) Shutdown(stopCh chan<- struct{}) {
+	r.queue.ShutDown()
+	close(stopCh)
 }
 
-func (w *K8sResourceWatcher) HasSynced() bool {
-	return true
+func (r *K8sResourceWatcher) HasSynced() bool {
+	return r.informer.HasSynced()
 }
