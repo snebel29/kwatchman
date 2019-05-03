8f11d26790af17140e372490e127395fa9572de3
Sven Nebel <nebel.sven@gmail.com>
2019-05-03 14:55:05 +0100

    Split resources and resources_watcher

--- internal/pkg/watcher/k8s/resources/resources.go
+++ internal/pkg/watcher/k8s/resources/resources.go
@@ -2,62 +2,21 @@ package resources
 
 import (
 	"context"
-	"fmt"
 	log "github.com/sirupsen/logrus"
-	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 
 	"github.com/snebel29/kooper/operator/common"
-	"github.com/snebel29/kooper/operator/controller"
 	"github.com/snebel29/kooper/operator/handler"
 	"github.com/snebel29/kooper/operator/retrieve"
 	"github.com/snebel29/kwatchman/internal/pkg/watcher"
 )
-
-type K8sResourceWatcher struct {
-	kind  string
-	stopC chan struct{}
-	ctrl  controller.Controller
-}
-
-func (r *K8sResourceWatcher) Run() error {
-	log.Printf("Run K8sResourceWatcher with kind %v\n", r.kind)
-
-	// Start our controller.
-	if err := r.ctrl.Run(r.stopC); err != nil {
-		return fmt.Errorf("error running controller: %s", err)
-	}
-	return nil
-}
-
-func (r *K8sResourceWatcher) Shutdown() {
-	log.Printf("Shutdown K8sResourceWatcher with kind %v\n", r.kind)
-	r.stopC <- struct{}{} //FIX: It's not really stopping the kooper controller
-}
-
-func newK8sResourceWatcher(kind string, hand *handler.HandlerFunc, retr *retrieve.Resource) watcher.ResourceWatcher {
-	// Create the controller that will refresh every 30 seconds.
-	ctrl := newK8sController(hand, retr)
-	stopC := make(chan struct{})
-
-	return &K8sResourceWatcher{
-		kind:  kind,
-		ctrl:  ctrl,
-		stopC: stopC,
-	}
-}
-
-func newK8sController(hand *handler.HandlerFunc, retr *retrieve.Resource) controller.Controller {
-	refresh := 30 * time.Second
-	logger := log.New()
-	return controller.NewSequential(refresh, hand, retr, nil, logger)
-}
 
 func NewK8sDeploymentWatcher(clientset kubernetes.Interface) watcher.ResourceWatcher {
 	kind := "Deployment"
--- internal/pkg/watcher/k8s/resources/resources_test.go
+++ internal/pkg/watcher/k8s/resources/resources_test.go
@@ -1,58 +1,24 @@
 package resources
 
 import (
-	"sync"
+	"github.com/snebel29/kwatchman/internal/pkg/watcher"
 	"testing"
-	//"sync"
-	"time"
 )
 
-var runCalled bool
+func k8sIndividualResourceWatcherHelper(w watcher.ResourceWatcher, t *testing.T) {
+	rw := w.(*K8sResourceWatcher)
 
-type KooperControllerMock struct{}
-
-func (w *KooperControllerMock) Run(stopper <-chan struct{}) error {
-	runCalled = true
-	return nil
+	if rw.kind == "" {
+		t.Errorf("kind should be != than \"\"")
+	}
+	if rw.ctrl == nil {
+		t.Errorf("ctrl should be != nil")
+	}
+	if rw.stopC == nil {
+		t.Errorf("stopC should be != nil")
+	}
 }
 
 func TestK8sDeploymentWatcher(t *testing.T) {
-	// Test that
-	w := NewK8sDeploymentWatcher(nil)
-	dw := w.(*K8sResourceWatcher)
-	if dw.kind == "" {
-		t.Errorf("kind should be != than \"\"")
-	}
-	if dw.ctrl == nil {
-		t.Errorf("ctrl should be != nil")
-	}
-	if dw.stopC == nil {
-		t.Errorf("stopC should be != nil")
-	}
-
-	// Test Shutdown() sends empty struct through stopC
-	stopC := make(chan struct{})
-	dw.stopC = stopC
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		select {
-		case <-stopC:
-		case <-time.After(1 * time.Second):
-			t.Errorf("Timed out while waiting for stopC signal")
-		}
-	}()
-
-	dw.Shutdown()
-	wg.Wait()
-
-	//Test Run() starts controller by calling ctrl.Run()
-	dw.ctrl = &KooperControllerMock{}
-	dw.Run()
-	if runCalled == false {
-		t.Error("ctrl.Run() should have been called")
-	}
+	k8sIndividualResourceWatcherHelper(NewK8sDeploymentWatcher(nil), t)
 }
