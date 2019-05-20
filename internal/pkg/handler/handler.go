b8c43390b36978ee01ecaa0cb44edf8ec96949cb
Sven Nebel <nebel.sven@gmail.com>
2019-05-20 14:30:28 +0100

    pretty print json

--- internal/pkg/handler/handler.go
+++ internal/pkg/handler/handler.go
@@ -1,15 +1,19 @@
 package handler
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	log "github.com/sirupsen/logrus"
 	"github.com/snebel29/kooper/operator/common"
 )
 
-type ResourcesHandlerFunc func(context.Context, *common.K8sEvent, string) error
+type ResourcesHandlerFunc func(context.Context, *common.K8sEvent, []byte) error
 
-func LogHandlerFunc(ctx context.Context, evt *common.K8sEvent, k8sManifest string) error {
+func LogHandlerFunc(ctx context.Context, evt *common.K8sEvent, k8sManifest []byte) error {
 	_ = ctx
-	log.Infof("Added: %v %s\n", evt.HasSynced, k8sManifest)
+	var indented bytes.Buffer
+	json.Indent(&indented, k8sManifest, "", " ")
+	log.Infof("Added: %v %s\n", evt.HasSynced, &indented)
 	return nil
 }
