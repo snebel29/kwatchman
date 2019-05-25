cc8463598757e76eb1cf25eb4df8bc4f889fd446
Sven Nebel <nebel.sven@gmail.com>
2019-05-25 20:36:30 +0100

    Move diff tests on its own file

--- internal/pkg/handler/handler_test.go
+++ internal/pkg/handler/handler_test.go
@@ -1,12 +1,12 @@
 package handler
 
 import (
-	"fmt"
 	log_test "github.com/sirupsen/logrus/hooks/test"
 	"github.com/snebel29/kooper/operator/common"
 	"testing"
 )
 
+// This test case effectively test both LogHandlerFunc and prettyPrintJSON
 func TestPrettyPrintJSON(t *testing.T) {
 
 	hook := log_test.NewGlobal()
@@ -17,15 +17,5 @@ func TestPrettyPrintJSON(t *testing.T) {
 
 	if m != prettyPrintJSON([]byte(s)) {
 		t.Errorf("%s should match %s", m, s)
-	}
-}
-
-func TestDiffHandlerUseSingletonStorage(t *testing.T) {
-	s1 := newStorage()
-	addr1 := fmt.Sprintf("%p", s1)
-	s2 := newStorage()
-	addr2 := fmt.Sprintf("%p", s2)
-	if addr1 != addr2 {
-		t.Errorf("addr should be the same however %s != %s", addr1, addr2)
 	}
 }
