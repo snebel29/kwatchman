476f48a2e4fd81602d11deec6e294fc906120b1d
Sven Nebel <nebel.sven@gmail.com>
2019-06-18 15:45:32 +0100

    Format slack msg further

--- internal/pkg/handler/slack/slack.go
+++ internal/pkg/handler/slack/slack.go
@@ -39,7 +39,7 @@ func truncateString(text string, limit int) string {
 }
 
 func MsgToSlack(kind, objKey, clusterName, resourceKind, payload string) error {
-	title := fmt.Sprintf("%s %s %s", strings.ToUpper(kind), resourceKind, objKey)
+	title := fmt.Sprintf("%s %s\n%s", strings.ToUpper(kind), resourceKind, objKey)
 
 	// From Aug-2018 Slack requires text field to be under 4000 characters
 	// https://api.slack.com/changelog/2018-04-truncating-really-long-messages
