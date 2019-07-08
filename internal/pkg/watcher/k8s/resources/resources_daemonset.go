f4f4d0ca606a5d54ed300fe6e8115a0593d3c64c
Sven Nebel <nebel.sven@gmail.com>
2019-07-08 14:50:40 +0100

    Add daemonset resource

---
+++ internal/pkg/watcher/k8s/resources/resources_daemonset.go
@@ -0,0 +1,44 @@
+package resources
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/tools/cache"
+
+	"github.com/snebel29/kooper/operator/retrieve"
+	"github.com/snebel29/kwatchman/internal/pkg/registry"
+	"github.com/snebel29/kwatchman/internal/pkg/watcher"
+)
+
+const (
+	DAEMONSET = "daemonset"
+)
+
+func init() {
+	registry.Register(registry.RESOURCES, DAEMONSET, NewDaemonsetWatcher)
+}
+
+// NewDaemonsetWatcher return a watcher for k8s daemonset
+func NewDaemonsetWatcher(arg ResourceWatcherArgs) watcher.ResourceWatcher {
+
+	resourceKind := DAEMONSET
+
+	retr := &retrieve.Resource{
+		Object: &appsv1.DaemonSet{},
+		ListerWatcher: &cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return arg.Clientset.AppsV1().DaemonSets(arg.Namespace).List(options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return arg.Clientset.AppsV1().DaemonSets(arg.Namespace).Watch(options)
+			},
+		},
+	}
+
+	return newK8sResourceWatcher(
+		resourceKind, newResourceHandlerFunc(arg.ChainOfHandlers, resourceKind),
+		retr)
+}
